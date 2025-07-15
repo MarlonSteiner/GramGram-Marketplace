025eaa79ec634340ad9d9700d92dfe8dffdbebea
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-07-15 17:51:02 +0200

    created reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,31 @@
 class ReviewsController < ApplicationController
   def new
-    @Review = Review.new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.granny = @granny
+    if @review.save
+      redirect_to granny_path(@granny)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def rating
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to granny_path(@review.granny), status: :see_other
+  end
+
+  private
+
+  def set_granny
+    @granny = Granny.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
